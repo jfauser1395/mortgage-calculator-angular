416931b9c5754b3c45d1dcd6f1f11bb5b7ce018b
jurifauser1395 <jurifauser@pm.me>
2024-01-18 20:48:29 +0100

    move legacy code to Angular environment

---
+++ app.ts
@@ -0,0 +1,8 @@
+function getFirstElement<ElemenType>(array: ElemenType[]) {
+    return array[0]
+}
+const numbers = [1, 2, 3]
+const firstNum = getFirstElement(numbers)
+
+const string = ['hi', 'by', 'me']
+const firstString = getFirstElement(string)
