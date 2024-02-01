9d47b97b994566617d06c22b419d56b435a45c9f
jurifauser1395 <jurifauser@pm.me>
2024-02-01 16:41:25 +0100

    testing commit

--- ngpro/src/app/monthly-mortgage-calculation.service.ts
+++ ngpro/src/app/monthly-mortgage-calculation.service.ts
@@ -6,12 +6,12 @@ import { Injectable } from '@angular/core';
 export class MonthlyMortgageCalculationService {
   // Declare the monthlyMortgagePayment function to calculate the monthly payment to the second decimal number
 monthlyMortgagePayment(principalLoan:number, rate:number, numberOfPayments:number) {
-  // P is the principal loan amount
-  // r is the monthly interest rate
-  // n is the number of payments
+    // P is the principal loan amount
+    // r is the monthly interest rate
+    // n is the number of payments
 
-  // The formula to calculate the monthly payment is (P * r) / (1 - (1 + r) ^ -n)
-  // The Math.pow function is used to calculate (1 + r) to the power of -n
-  return  (principalLoan * rate) / (1 - Math.pow(1 + rate, -numberOfPayments));
-}
+    // The formula to calculate the monthly payment is (P * r) / (1 - (1 + r) ^ -n)
+    // The Math.pow function is used to calculate (1 + r) to the power of -n
+    return  (principalLoan * rate) / (1 - Math.pow(1 + rate, -numberOfPayments));
+  }
 }
